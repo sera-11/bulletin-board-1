8282f9db3492019b37f517db2e5087a44acfbed2
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-02-13 19:13:55 +0000

    deleted posts after bash: rails destroy draft:resource post

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_13_191025) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.date "expires_on"
-    t.integer "board_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
